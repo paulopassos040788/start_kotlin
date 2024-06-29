96af89d5eae33302e71bf592ec7ee879ce83f13e
Paulo César dos Passos Pereira <paulo.passos1@gmail.com>
2024-06-29 19:21:51 -0300

    Primeiro hello world

---
+++ src/helloWord.kt
@@ -0,0 +1,14 @@
+fun main(){
+    println("Hello Kotlin")
+
+    // variaveis locais
+    val number1 = 5 // variavel somente de leitura
+    var number2 = 10 // variavel mutavel
+
+    println("Variavel 1: $number1")
+    println("Variavel 2: $number2")
+    println("Variavel 3: $number3")
+
+}
+
+const val number3 = 20 // variavel global
