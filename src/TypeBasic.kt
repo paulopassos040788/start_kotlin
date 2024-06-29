bce6e310c1ecb7fb2be8ef6d5730df986702a8f6
Paulo César dos Passos Pereira <paulo.passos1@gmail.com>
2024-06-29 19:46:39 -0300

    Tipos basicos do kotlin

---
+++ src/TypeBasic.kt
@@ -0,0 +1,20 @@
+fun main(){
+    val number1: Int = 10
+    val number2: Float = 10.00f
+    val number3: Double = 50.00
+    val booleanTrue: Boolean = true
+    val string1: String = "Uma string aqui!!!"
+    number4 *= 800
+    //number4 /= 800
+    //number4 += 800
+    //number4 -= 800
+
+    println(number1)
+    println(number2)
+    println(number3)
+    println(number4)
+    println(string1)
+    println(booleanTrue)
+}
+
+var number4: Int = 100
